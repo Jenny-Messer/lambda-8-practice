95026fa3b6cf85c54b192e5e5d6d200e98157a28
jennifermesser <jennifer.messer@travelex.com>
2018-07-23 11:04:07 +0100

    implement lambda version of previous code

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
-        HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
 
-        //greeter performs an action, helloworldgreeting is an object that holds that action
-        greeter.greet(helloWorldGreeting);
+        Greeting helloWorldGreeting = new HelloWorldGreeting();
+
+        //replaces helloWorldGreeting
+        Runnable lambdaGreeting = () -> System.out.println("Hello World!");
+
+        /*
+
+        Runnable is a functional interface built into java 8
+        Runnable takes no args and returns nothing, it just runs what it is given
+
+         */
+
+        //these are two ways of doing the same thing
+        helloWorldGreeting.perform();
+        lambdaGreeting.run();
+
     }
 }
