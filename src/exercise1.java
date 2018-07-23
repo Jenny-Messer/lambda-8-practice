b1d3619e0e7e26bdb1421296de3974c0fa1e012d
jennifermesser <jennifer.messer@travelex.com>
2018-07-23 14:03:33 +0100

    done exercise

--- src/exercise1.java
+++ src/exercise1.java
@@ -1,7 +1,10 @@
 // Copyright (c) 2018 Travelex Ltd
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class exercise1 {
 
@@ -15,12 +18,42 @@ public class exercise1 {
                 new Person("Matthew", "Arnold", 39)
         );
 
-        //TODO sort list by name
-        //TODO create a method that prints all elements in the list
+        //TODO sort list by last name
+        //non lambda
+        Collections.sort(people, new Comparator<Person>() {
+
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getLastName().compareTo(o2.getLastName());
+            }
+        });
+
+        //lambda version
+        Collections.sort(people, (p1, p2) -> p1.getLastName().compareTo(p2.getLastName()));
+
+        //TODO print
+        //non lambda
+        for (Person p : people){
+            System.out.println(p);
+        }
+
+        //lambda
+        printConditionally(people, p -> true);
+
         //TODO create method that prints all people that have last name beginning with C
+        printConditionally(people, p -> p.getFirstName().startsWith("C"));
 
 
+    }
 
+    //Predicate<Person> is a lambda that takes 1 argument of type Person
+    //and has the method test() which returns a boolean
+    private static void printConditionally(List<Person> people, Predicate<Person> condition){
+        for (Person p : people) {
+            if (condition.test(p)) {
+                System.out.println(p);
+            }
+        }
     }
 
 }
