b61a92301e44407d4c90cd129f8111a8c9e1618a
jennifermesser <jennifer.messer@travelex.com>
2018-07-24 11:30:37 +0100

    added exceptionHandlingExample

---
+++ src/JavaBrains/ExceptionHandlingExample.java
@@ -0,0 +1,37 @@
+// Copyright (c) 2018 Travelex Ltd
+
+package JavaBrains;
+
+import java.util.function.BiConsumer;
+
+public class ExceptionHandlingExample {
+
+    public static void main(String[] args) {
+        int [] someNumbers = {1, 2, 3, 4};
+        int key = 0;
+
+        process(someNumbers, key, wrapperLambda((v, k) -> System.out.println( v / k )));
+
+    }
+
+    private static void process (int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer){
+        for (int i : someNumbers){
+            consumer.accept(i, key);
+        }
+    }
+
+    //moves try catch out of lambda so it can stay one line
+    private static BiConsumer<Integer, Integer> wrapperLambda(BiConsumer<Integer, Integer> consumer) {
+        return (v, k) -> {
+
+            try {
+                consumer.accept(v, k);
+            }
+            catch (ArithmeticException e) {
+                System.out.println("Exception caught in wrapper lambda");
+            }
+
+        };
+    }
+
+}
